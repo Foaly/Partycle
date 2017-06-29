9d374e65184cbccafcf5333e37dcdd7acd03afac
Anyere Bendrien <anyere.bendrien@posteo.de>
2017-06-29 23:43:07 +0200

    Add 'fade-system'

--- src/main.rs
+++ src/main.rs
@@ -49,16 +49,54 @@ impl Component for Particle {
 }
 
 
+#[derive(Debug)]
+struct Lifetime {
+    pub acc: f32,
+    pub inc: f32,
+    pub max: f32,
+}
 
-struct RendererSystem<'a> {
+impl Lifetime {
+    pub fn new() -> Self {
+        Lifetime {
+            acc: 0.0,
+            inc: 0.03,
+            max: 10.0,
+        }
+    }
+}
+
+impl Component for Lifetime {
+    type Storage = VecStorage<Self>;
+}
+
+
+struct FadeSystem;
+
+impl<'a> System<'a> for FadeSystem {
+    type SystemData = (WriteStorage<'a, Particle>, WriteStorage<'a, Lifetime>);
+
+    fn run(&mut self, (mut particles, mut lifetimes): Self::SystemData) {
+        for (particle, lifetime) in (&mut particles, &mut lifetimes).join() {
+            let alpha = ((1.0 - (lifetime.acc / lifetime.max)) * std::u8::MAX as f32) as u8;
+            for i in 0..particle.vertices.len() {
+                particle.vertices[i].color.a = alpha;
+            }
+            lifetime.acc += lifetime.inc;
+        }
+    }
+}
+
+
+struct RenderSystem<'a> {
     render_target: Rc<RefCell<&'a mut RenderWindow>>,
 }
 
-impl<'a, 'b> System<'a> for RendererSystem<'b> {
-    type SystemData = ((), ReadStorage<'a, Particle>);
+impl<'a, 'b> System<'a> for RenderSystem<'b> {
+    type SystemData = ReadStorage<'a, Particle>;
 
-    fn run(&mut self, (_, particles): Self::SystemData) {
-        for particle in (&particles).join() {
+    fn run(&mut self, particles: Self::SystemData) {
+        for particle in particles.join() {
             self.render_target.borrow_mut().draw(particle)
         }
     }
@@ -66,28 +104,35 @@ impl<'a, 'b> System<'a> for RendererSystem<'b> {
 
 
 fn main() {
-    let videoMode = sfml::window::VideoMode::new(800, 600, 32);
-    let mut window = RenderWindow::new(videoMode,
+    let video_mode = sfml::window::VideoMode::new(800, 600, 32);
+    let mut window = RenderWindow::new(video_mode,
                                        "SFML VertexArray accessors Example",
                                        style::DEFAULT,
                                        &Default::default());
     window.set_vertical_sync_enabled(true);
-
-    let wrapper = Rc::new(RefCell::new(&mut window));
-    let renderer = RendererSystem {render_target: wrapper.clone()};
+    let render_wrapper = Rc::new(RefCell::new(&mut window));
 
     let mut world = World::new();
+
+    // components
     world.register::<Particle>();
+    world.register::<Lifetime>();
 
+    // entities
+    world.create_entity()
+        .with(Particle::new())
+        .with(Lifetime::new())
+        .build();
 
-    world.create_entity().with(Particle::new()).build();
-
+    // systems
+    let rander_system = RenderSystem {render_target: render_wrapper.clone()};
     let mut dispatcher = DispatcherBuilder::new()
-        .add_thread_local(renderer)
+        .add_thread_local(FadeSystem)
+        .add_thread_local(rander_system)
         .build();
 
     loop {
-        for e in wrapper.borrow().events() {
+        for e in render_wrapper.borrow().events() {
             match e {
                 Event::Closed => return,
                 Event::KeyReleased { code: keycode, .. } => match keycode {
@@ -98,14 +143,13 @@ fn main() {
             }
         }
 
+        // clear the window
+        render_wrapper.borrow_mut().clear(&Color::black());
 
-        // Clear the window
-        wrapper.borrow_mut().clear(&Color::black());
-
+        // update world
         dispatcher.dispatch(&mut world.res);
 
-        //window.draw(&particle);
-        // Display things on screen
-        wrapper.borrow_mut().display()
+        // display things on screen
+        render_wrapper.borrow_mut().display();
     }
 }
