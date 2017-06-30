3f44e57cce3ab279a6a9acbe0b2684c5dbbde6a7
Anyere Bendrien <anyere.bendrien@posteo.de>
2017-06-30 02:23:19 +0200

    :3

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,38 @@
 extern crate sfml;
 extern crate specs;
+extern crate rand;
 
-use sfml::graphics::{Color, PrimitiveType, RenderTarget, RenderWindow, Vertex, Drawable, RenderStates};
+use sfml::graphics::{Color, PrimitiveType, RenderTarget, RenderWindow, Vertex, Drawable,
+                     RenderStates, Transform as SfmlTransform};
 use sfml::window::{Event, style, Key};
+use sfml::system::Vector2f;
 
 use specs::{Component, DispatcherBuilder, Join, ReadStorage, System, VecStorage, World,
             WriteStorage};
 
+use rand::distributions::{IndependentSample, Range};
+
 use std::cell::RefCell;
 use std::rc::Rc;
 
+const WIDTH: u32 = 1920;
+const HEIGHT: u32 = 1080;
+const QUAD: f32 = 2.0;
+const ENTITIES: usize = 10000;
+
+struct Transform(SfmlTransform);
+
+impl Component for Transform {
+    type Storage = VecStorage<Self>;
+}
+
 
 #[derive(Debug)]
-struct Particle {
+struct Quad {
     pub vertices : [Vertex; 6]
 }
 
-impl Drawable for Particle {
+impl Drawable for Quad {
     fn draw<'se, 'tex, 'sh, 'shte>(&'se self,
                                    target: &mut RenderTarget,
                                    states: RenderStates<'tex, 'sh, 'shte>)
@@ -26,25 +42,42 @@ impl Drawable for Particle {
     }
 }
 
-impl Particle {
+impl Quad {
     pub fn new() -> Self {
-        let mut vertices = [Vertex::default(); 6];
-        vertices[0] = Vertex::with_pos_color((0.0, 0.0), Color::red());
-        vertices[1] = Vertex::with_pos_color((100.0, 0.0), Color::red());
-        vertices[2] = Vertex::with_pos_color((0.0, 100.0), Color::red());
+        let mut quad = Quad {
+            vertices: [Vertex::default(); 6]
+        };
+        quad.rand_pos();
+        quad.rand_color();
+        quad
+    }
 
-        vertices[3] = Vertex::with_pos_color((100.0, 0.0), Color::green());
-        vertices[4] = Vertex::with_pos_color((100.0, 100.0), Color::green());
-        vertices[5] = Vertex::with_pos_color((0.0, 100.0), Color::green());
+    pub fn rand_pos(&mut self) {
+        let x_rng = Range::new(0.0, WIDTH as f32 - QUAD);
+        let y_rng = Range::new(0.0, HEIGHT as f32 - QUAD);
+        let mut rng = rand::thread_rng();
+        let (x, y) = (x_rng.ind_sample(&mut rng), y_rng.ind_sample(&mut rng));
+        self.vertices[0].position = (x, y).into();
+        self.vertices[1].position = (x + QUAD, y).into();
+        self.vertices[2].position = (x, y + QUAD).into();
+        self.vertices[3].position = (x + QUAD, y).into();
+        self.vertices[4].position = (x + QUAD, y + QUAD).into();
+        self.vertices[5].position = (x, y + QUAD).into();
+    }
 
-
-        Particle {
-            vertices: vertices
+    pub fn rand_color(&mut self) {
+        let c_rng = Range::new(0u8, std::u8::MAX);
+        let mut rng = rand::thread_rng();
+        let (r, g, b) = (c_rng.ind_sample(&mut rng), c_rng.ind_sample(&mut rng), c_rng.ind_sample(&mut rng));
+        for i in 0..self.vertices.len() {
+            self.vertices[i].color.r = r;
+            self.vertices[i].color.g = g;
+            self.vertices[i].color.b = b;
         }
     }
 }
 
-impl Component for Particle {
+impl Component for Quad {
     type Storage = VecStorage<Self>;
 }
 
@@ -58,10 +91,13 @@ struct Lifetime {
 
 impl Lifetime {
     pub fn new() -> Self {
+        let inc_rng = Range::new(0.1, 1.0);
+        let max_rng = Range::new(10.0, 100.0);
+        let mut rng = rand::thread_rng();
         Lifetime {
             acc: 0.0,
-            inc: 0.03,
-            max: 10.0,
+            inc: inc_rng.ind_sample(&mut rng),
+            max: max_rng.ind_sample(&mut rng),
         }
     }
 }
@@ -74,7 +110,7 @@ impl Component for Lifetime {
 struct FadeSystem;
 
 impl<'a> System<'a> for FadeSystem {
-    type SystemData = (WriteStorage<'a, Particle>, WriteStorage<'a, Lifetime>);
+    type SystemData = (WriteStorage<'a, Quad>, WriteStorage<'a, Lifetime>);
 
     fn run(&mut self, (mut particles, mut lifetimes): Self::SystemData) {
         for (particle, lifetime) in (&mut particles, &mut lifetimes).join() {
@@ -82,7 +118,13 @@ impl<'a> System<'a> for FadeSystem {
             for i in 0..particle.vertices.len() {
                 particle.vertices[i].color.a = alpha;
             }
+
             lifetime.acc += lifetime.inc;
+            if lifetime.acc >= lifetime.max {
+                particle.rand_pos();
+                particle.rand_color();
+                lifetime.acc = 0.0;
+            }
         }
     }
 }
@@ -93,7 +135,7 @@ struct RenderSystem<'a> {
 }
 
 impl<'a, 'b> System<'a> for RenderSystem<'b> {
-    type SystemData = ReadStorage<'a, Particle>;
+    type SystemData = ReadStorage<'a, Quad>;
 
     fn run(&mut self, particles: Self::SystemData) {
         for particle in particles.join() {
@@ -104,35 +146,38 @@ impl<'a, 'b> System<'a> for RenderSystem<'b> {
 
 
 fn main() {
-    let video_mode = sfml::window::VideoMode::new(800, 600, 32);
+    let video_mode = sfml::window::VideoMode::new(WIDTH, HEIGHT, 32);
     let mut window = RenderWindow::new(video_mode,
                                        "SFML VertexArray accessors Example",
                                        style::DEFAULT,
                                        &Default::default());
     window.set_vertical_sync_enabled(true);
-    let render_wrapper = Rc::new(RefCell::new(&mut window));
+    let window_wrapper = Rc::new(RefCell::new(&mut window));
 
     let mut world = World::new();
 
     // components
-    world.register::<Particle>();
+    world.register::<Transform>();
+    world.register::<Quad>();
     world.register::<Lifetime>();
 
     // entities
-    world.create_entity()
-        .with(Particle::new())
-        .with(Lifetime::new())
-        .build();
+    for _ in 0..ENTITIES {
+        world.create_entity()
+            .with(Quad::new())
+            .with(Lifetime::new())
+            .build();
+    }
 
     // systems
-    let rander_system = RenderSystem {render_target: render_wrapper.clone()};
+    let rander_system = RenderSystem {render_target: window_wrapper.clone()};
     let mut dispatcher = DispatcherBuilder::new()
         .add_thread_local(FadeSystem)
         .add_thread_local(rander_system)
         .build();
 
     loop {
-        for e in render_wrapper.borrow().events() {
+        for e in window_wrapper.borrow().events() {
             match e {
                 Event::Closed => return,
                 Event::KeyReleased { code: keycode, .. } => match keycode {
@@ -144,12 +189,13 @@ fn main() {
         }
 
         // clear the window
-        render_wrapper.borrow_mut().clear(&Color::black());
+        window_wrapper.borrow_mut().clear(&Color::black());
 
         // update world
         dispatcher.dispatch(&mut world.res);
+        world.maintain();
 
         // display things on screen
-        render_wrapper.borrow_mut().display();
+        window_wrapper.borrow_mut().display();
     }
 }
